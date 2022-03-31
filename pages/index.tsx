d2c62941262ee0e7d7cf74114f07e643badd7a83
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2022-03-31 20:14:47 +0900

    Added buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,70 @@ import Head from "next/head";
 import { useCallback, useRef, useState } from "react";
 import Webcam from "react-webcam";
 import Image from "next/image";
+import liff from "@line/liff";
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+const SendMessageButton = () => {
+  /* 追加: メッセージ送信 */
+  const sendMessage = () => {
+    liff
+      .init({ liffId: process.env.REACT_APP_LIFF_ID as string }) // LIFF IDをセットする
+      .then(() => {
+        if (!liff.isLoggedIn()) {
+          liff.login({}); // ログインしていなければ最初にログインする
+        } else if (liff.isInClient()) {
+          // LIFFので動いているのであれば
+          liff
+            .sendMessages([
+              {
+                // メッセージを送信する
+                type: "text",
+                text: "成功したぜ！Oh Yeah!! You've successfully sent a message! Hooray!",
+              },
+            ])
+            .then(function () {
+              window.alert("Message sent");
+            })
+            .catch(function (error) {
+              window.alert("Error sending message: " + error);
+            });
+        }
+      });
+  };
+  return (
+    <button className="button" onClick={sendMessage}>
+      SendMessage
+    </button>
+  );
+};
+
+const ShowProfileButton = () => {
+  /* 追加: UserProfileをAlertで表示 */
+  const getUserInfo = () => {
+    liff.init({ liffId: process.env.REACT_APP_LIFF_ID as string }).then(() => {
+      if (!liff.isLoggedIn()) {
+        liff.login({}); // ログインしていなければ最初にログインする
+      } else if (liff.isInClient()) {
+        liff
+          .getProfile() // ユーザ情報を取得する
+          .then((profile) => {
+            const userId: string = profile.userId;
+            const displayName: string = profile.displayName;
+            alert(`Name: ${displayName}, userId: ${userId}`);
+          })
+          .catch(function (error) {
+            window.alert("Error sending message: " + error);
+          });
+      }
+    });
+  };
+  return (
+    <button className="button" onClick={getUserInfo}>
+      Show user info
+    </button>
+  );
+};
 
 const videoConstraints = {
   width: 720,
@@ -12,7 +74,7 @@ const videoConstraints = {
   facingMode: "user",
 };
 
-export const Camera = () => {
+const Camera = () => {
   const [isCaptureEnable, setCaptureEnable] = useState<boolean>(false);
   const webcamRef = useRef<Webcam>(null);
   const [url, setUrl] = useState<string | null>(null);
@@ -28,6 +90,9 @@ export const Camera = () => {
       <header>
         <h1>Camera</h1>
       </header>
+      <ShowProfileButton />
+      &nbsp;&nbsp;
+      <SendMessageButton />
       {isCaptureEnable || (
         <button onClick={() => setCaptureEnable(true)}>Launch</button>
       )}
@@ -61,7 +126,7 @@ export const Camera = () => {
             </button>
           </div>
           <div>
-            <Image src={url} alt="Screenshot" />
+            <Image src={url} alt="Screenshot" width={540} height={360} />
           </div>
         </>
       )}
